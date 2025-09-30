79e47b52df863a896b72f486229bd150a860c196
Bennor McCarthy <bennor.mccarthy@vercel.com>
2025-09-30 11:01:51 +1000

    Set _request_ headers.

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,15 @@ export function middleware(request: NextRequest) {
   const proxiedOrigin = process.env.PROXIED_ORIGIN ?? "";
   const bypassToken = process.env.BYPASS_TOKEN ?? "";
   const pathAndQuery = request.nextUrl.href.replace(request.nextUrl.origin, "");
+
   const url = new URL(pathAndQuery, proxiedOrigin);
-  console.log(url.href);
+
+  const headers = new Headers(request.headers);
+  headers.set("x-vercel-protection-bypass", bypassToken);
+
   return NextResponse.rewrite(url, {
-    headers: {
-      "x-vercel-protection-bypass": bypassToken,
+    request: {
+      headers,
     },
   });
 }
